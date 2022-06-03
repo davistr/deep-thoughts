963b3fc401348fb6916c3f2e87fddfb9fcd52ad8
davistr <davistr1216@gmail.com>
2022-06-02 20:57:04 -0400

    setup full stack structure

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { authMiddleware } = require("./utils/auth");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -38,4 +39,13 @@ db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
+});
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
